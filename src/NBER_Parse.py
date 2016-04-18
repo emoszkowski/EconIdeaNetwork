2057d0da1570744e901e0b30d48d0ba621d21c44
Ron Yang <ron@rnyang.com>
2016-04-17 23:24:19 -0400

    add parsing of JEL codes into graph

--- src/NBER_Parse.py
+++ src/NBER_Parse.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import networkx as nx
 import numpy as np
+import nltk.metrics as nm
 
 ######################################
 # Read file from pipe-delimited file #
@@ -9,11 +10,18 @@ import numpy as np
 filepath = '../save/NBER_Paper_Info.1.csv'
 nber = pd.read_csv(filepath, delimiter='|')
 
-# First step: Create coauthorship graph
+####################
+# Initial Cleaning #
+####################
 
-# Convert nber authors into edgelist
+# Drop 'Jr.'
+nber.authors = nber.authors.apply(lambda x: x.replace(', Jr.',''))
+nber.authors = nber.authors.apply(lambda x: x.replace(', Jr',''))
+nber.authors = nber.authors.apply(lambda x: x.replace(' Jr.',''))
+nber.authors = nber.authors.apply(lambda x: x.replace(' Jr',''))
+
+# Separate List of Authors
 authorLists = nber.authors.apply(lambda x: x.split(','))
-
 maxLength = max(authorLists.apply(lambda x: len(x)))
 
 def safeindex(x, i):
@@ -25,21 +33,54 @@ def safeindex(x, i):
 for i in range(maxLength):
     nber['author' + str(i)] = authorLists.apply(lambda x: safeindex(x,i))
 
-# reshape to long
+#####################################
+# Create long table of paper-author #
+#####################################
+
 cols = ['title'] + [c for c in nber.columns if \
                     'author' in c and 'authors' not in c]
 nberLong = pd.melt(nber[cols], id_vars=['title'])
 nberLong = nberLong[~pd.isnull(nberLong.value)][['title','value']]
-nberLong.value = nberLong.value.apply(lambda x: x.replace(' ',''))
 
-# create Cartesian product
+# Drop middle names and punctuation 
+# b/c they throw off
+def dropMiddle(x):
+    xSplit = x.strip().split(' ')
+    if len(xSplit) < 3:
+        return ''.join(xSplit)
+    else:
+        return ''.join([xSplit[0],xSplit[-1]])
+nberLong.value = nberLong.value.apply(dropMiddle)
+nberLong.value = nberLong.value.apply(lambda x: x.replace('.',''))
+
+#######################################
+# create Cartesian product to extract #
+# coauthorship graph edges            #
+#######################################
+
 nberCart = pd.merge(nberLong, nberLong, how='outer', on='title')
 nberCart = nberCart[nberCart.value_x != nberCart.value_y]
 
-# Create Edge List
-nberEdges = list(nberCart.value_x + '|' + nberCart.value_y)
-G = nx.parse_edgelist(nberEdges, delimiter='|')
+#######################################
+# Add JEL Codes to Coauthorship Edges #
+#######################################
 
-# Write to File
+nberCartJEL = pd.merge(nberCart, nber[['title','jel']], how='left', on='title')
+nberCartJEL.jel = nberCartJEL.jel.apply(lambda x: x.replace(', ',','))
+
+nberEdges = list(nberCartJEL.value_x + '|' + nberCartJEL.value_y + '|' + nberCartJEL.jel)
+
+################
+# Create Graph #
+################
+
+G = nx.parse_edgelist(nberEdges, delimiter='|', data=(('jelcode',str),))
+
+# Add Department Attribute (WIP)
+
+#################
+# Write to File #
+#################
+
 outpath = '../save/nber.graphml'
 nx.write_graphml(G, outpath)
